576b89597bfbe4aa087cb6236cc2ead34acd68a4
Jan Kalkan <jan.kalkan@mailbox.org>
2019-08-29 16:16:34 +0200

    Import Dask related classes into cluster package

--- lazycluster/cluster/__init__.py
+++ lazycluster/cluster/__init__.py
@@ -1,3 +1,5 @@
 from __future__ import absolute_import
 
 from lazycluster.cluster.runtime_cluster import MasterLauncher, WorkerLauncher, RuntimeCluster, MasterWorkerCluster
+
+from lazycluster.cluster.dask_cluster import LocalMasterLauncher, RoundRobinLauncher, DaskCluster
