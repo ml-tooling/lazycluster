62c8096698a4d6954b36289648d24ad79bd88d07
Jan Kalkan <jan.kalkan@mailbox.org>
2019-10-07 11:26:41 +0200

    Implements HyperoptCluster and RoundRobinLauncher

--- lazycluster/cluster/hyperopt_cluster.py
+++ lazycluster/cluster/hyperopt_cluster.py
@@ -1,7 +1,6 @@
 """Module for conveniently managing a Hyperopt cluster. https://github.com/hyperopt/hyperopt
 """
 
-from distributed import Client
 import time
 from typing import List, Union, Optional
 
@@ -68,7 +67,7 @@ class LocalMongoLauncher(MasterLauncher):
 
 
 class RoundRobinLauncher(WorkerLauncher):
-    """WorkerLauncher implementation for launching DASK workers in a round robin manner.
+    """WorkerLauncher implementation for launching hyperopt workers in a round robin manner.
     """
 
     def __init__(self, runtime_group: RuntimeGroup):
@@ -82,19 +81,16 @@ class RoundRobinLauncher(WorkerLauncher):
 
         self.log.debug('RoundRobinLauncher object created.')
 
-    def start(self, worker_count: int, master_port: int, ports: List[int]) -> List[int]:
+    def start(self, worker_count: int, master_port: int, ports: List[int] = None) -> List[int]:
         """Launches the worker instances in the `RuntimeGroup`.
 
         Args:
             worker_count: The number of worker instances to be started in the group.
             master_port:  The port of the master instance.
-            ports: The ports to be used for starting the workers. Only ports from the list will be chosen
-                               that are actually free.
+            ports: Without use here. Only here because we need to adhere to the interface defined by the
+                   WorkerLauncher class.
         Returns:
             List[int]: The updated port list after starting the workers, i.e. the used ones were removed.
-
-        Raises:
-            NoPortsLeftError: If there are not enough free ports for starting all workers.
         """
         self._ports = ports
         hosts = self._group.hosts
@@ -106,75 +102,54 @@ class RoundRobinLauncher(WorkerLauncher):
             runtime_index = (self._group.runtime_count + worker_index) % self._group.runtime_count
             # Get the actual host corresponding to the index
             host = hosts[runtime_index]
-            working_dir = runtimes[runtime_index].working_directory
+            # working_dir = runtimes[runtime_index].working_directory
             assert host == runtimes[runtime_index].host
 
-            self.log.debug(f'Launch Dask worker with index {worker_index} on Runtime {host}')
+            self.log.debug(f'Launch Hyperopt worker with index {worker_index} on Runtime {host}')
 
-            worker_port = self._launch_single_worker(host, worker_index, master_port, working_dir)  # NoPortsLeftError
-            # Remember which worker ports are now used per `Runtime`
-            if host in self._ports_per_host:
-                self._ports_per_host[host].append(worker_port)
-            else:
-                self._ports_per_host.update({host: [worker_port]})
-
-        # Needed for worker communication between workers, which is necessary in case of DASK
-        self.setup_worker_ssh_tunnels()
+            self._launch_single_worker(host, worker_index, master_port)
 
         return self._ports
 
-    def _launch_single_worker(self, host: str, worker_index: int, master_port: int, working_dir: str):
+    def _launch_single_worker(self, host: str, worker_index: int, master_port: int):
         """Launch a single worker instance in a `Runtime` in the `RuntimeGroup`.
-
-        Raises:
-            NoPortsLeftError
         """
-        # 1. Get a free port based on the port list
-        worker_port = self._group.get_free_port(self._ports)  # Raises NoPortsLeftError
-        self._ports = _utils.get_remaining_ports(self._ports, worker_port)
         # 2. Start the worker on this port
-        task = RuntimeTask('launch-dask-worker-' + str(worker_index))
-        task.run_command(DaskCluster.PIP_INSTALL_COMMAND)
-        task.run_command(self._get_launch_command(master_port, worker_port, working_dir))
+        task = RuntimeTask('launch-hyperopt-worker-' + str(worker_index))
+        task.run_command(self._get_launch_command(master_port))
         self._group.execute_task(task, host)
-        return worker_port
 
     @classmethod
-    def _get_launch_command(cls, master_port: int, worker_port: int, working_directory: str) -> str:
+    def _get_launch_command(cls, master_port: int, db: str = 'hyperopt', poll_intervall: float = 0.1) -> str:
         """Get the shell command for starting a worker instance.
 
         Returns:
              str: The launch command.
         """
-        return 'dask-worker --worker-port=' + str(worker_port) + ' --local-directory=' + working_directory \
-               + ' localhost:' + str(master_port)
+        return f'hyperopt-mongo-worker --mongo=localhost:{str(master_port)}/{db} --poll-interval={str(0.1)}'
 
 
 class Hyperoptluster(MasterWorkerCluster):
-    """Convenient class for launching a Dask cluster in a `RuntimeGroup`. 
+    """Convenient class for launching a Hyperopt cluster in a `RuntimeGroup`.
 
-    The number of DASK workers defaults to the number of `Runtimes` in the used `RuntimeGroup`. This number can be
+    The number of hyperopt workers defaults to the number of `Runtimes` in the used `RuntimeGroup`. This number can be
     adjusted so that more or less workers than available `Runtimes` can be used. Per default the desired number of
     workers is started in a round robin way as implemented in `RoundRobinLauncher`. Consequently, this leads to an
-    equal distribution of DASK workers in the `RuntimeGroup`. You can provide a custom implementation inheriting from
-    the `WorkerLauncher` class in order to execute a different strategy how workers should be started. The
-    DASK master (i.e. scheduler) will always be started on localhost as implemented in `LocalMasterLauncher`. This
+    equal distribution of hyperopt workers in the `RuntimeGroup`. You can provide a custom implementation inheriting
+    from the `WorkerLauncher` class in order to execute a different strategy how workers should be started. The
+    master instance (i.e. the mongoDB) will always be started on localhost as implemented in `LocalMasterLauncher`. This
     behavior can also be changed by providing a custom implementation inheriting from the `MasterLauncher`.
     """
 
-    DEFAULT_MASTER_PORT = 8786
-    PIP_INSTALL_COMMAND = 'pip install -q "dask[complete]"'
+    DEFAULT_MASTER_PORT = 1234
 
-    def __init__(self, runtime_group: RuntimeGroup, ports: Optional[List[int]] = None,
+    def __init__(self, runtime_group: RuntimeGroup,
                  master_launcher: Optional[MasterLauncher] = None,
                  worker_launcher: Optional[WorkerLauncher] = None):
         """Initialization method.
 
         Args:
-            runtime_group: The `RuntimeGroup` contains all `Runtimes` which can be used for starting the DASK entities.
-            ports: The list of ports which will be used to instantiate a cluster. Defaults to
-                                        list(range(self.DEFAULT_PORT_RANGE_START,
-                                                   self.DEFAULT_PORT_RANGE_END)).
+            runtime_group: The `RuntimeGroup` contains all `Runtimes` which can be used for starting the entities.
             master_launcher: Optionally, an instance implementing the `MasterLauncher` interface can be given, which
                              implements the strategy for launching the master instances in the cluster. If None, then
                              `LocalMasterLauncher` is used.
@@ -182,20 +157,10 @@ class Hyperoptluster(MasterWorkerCluster):
                              implements the strategy for launching the worker instances. If None, then
                              `RoundRobinLauncher` is used.
         """
-        super().__init__(runtime_group, ports)
+        super().__init__(runtime_group)
 
-        self._master_launcher = master_launcher if master_launcher else LocalMasterLauncher(runtime_group)
+        self._master_launcher = master_launcher if master_launcher else LocalMongoLauncher(runtime_group)
         self._worker_launcher = worker_launcher if worker_launcher else RoundRobinLauncher(runtime_group)
 
-        self.log.debug('DaskCluster initialized.')
+        self.log.debug('HyperoptCluster initialized.')
 
-    def get_client(self, timeout: int = 2) -> Client:
-        """Get a connected Dask client. 
-        
-        Args:
-            timeout: The timeout (s) value passed on to the Dask `Client` constructor. Defaults to 2.
-
-        Raises:
-            TimeoutError: If client connection `timeout` expires.
-        """
-        return Client('localhost:' + str(self.master_port), timeout=timeout)  # Raises TimeoutError
