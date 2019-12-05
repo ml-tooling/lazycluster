19ecbcafd7d5bfa21cdab8a5bbfa27123f879883
Jan Kalkan <jan.kalkan@mailbox.org>
2019-12-05 15:57:38 +0100

    Change import structure for cluster package

--- lazycluster/cluster/__init__.py
+++ lazycluster/cluster/__init__.py
@@ -1,5 +1,3 @@
 from __future__ import absolute_import
 
 from lazycluster.cluster.runtime_cluster import MasterLauncher, WorkerLauncher, RuntimeCluster, MasterWorkerCluster
-
-from lazycluster.cluster.dask_cluster import LocalMasterLauncher, RoundRobinLauncher, DaskCluster
