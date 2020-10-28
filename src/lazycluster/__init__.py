48343f080822ba11b824c2f49a4d1d51abdccffd
Jan Kalkan <jan.kalkan@mailbox.org>
2020-10-28 18:13:13 +0100

    Update __init__.py

--- src/lazycluster/__init__.py
+++ src/lazycluster/__init__.py
@@ -3,11 +3,10 @@ from lazycluster import about
 __version__ = about.__version__
 
 from lazycluster.exceptions import *
-
-# from lazycluster.runtime_mgmt import *
-from lazycluster.runtimes import *
+from lazycluster.runtime_mgmt import RuntimeGroup, RuntimeManager
+from lazycluster.runtimes import Runtime, RuntimeTask
 
 # from .settings import BRANCH, GITHUB_URL, PIP_PROJECT_NAME
-from .utils import Environment
+from lazycluster.utils import Environment
 
 Environment.set_third_party_log_level(Environment.third_party_log_level)
