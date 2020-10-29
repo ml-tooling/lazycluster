0c63691e1b36ded861f083cb93569b33c4d1e772
Jan Kalkan <jan.kalkan@mailbox.org>
2020-10-29 18:16:49 +0100

    Update build-env gh action

--- .github/actions/build-environment/entrypoint.sh
+++ .github/actions/build-environment/entrypoint.sh
@@ -3,20 +3,22 @@
 # Stops script execution if a command has an error
 set -e
 
-if [ -z "$GITHUB_TOKEN" ]; then
-    # If github token is provided via secret, it is in input github token.
-    GITHUB_TOKEN=$INPUT_GITHUB_TOKEN
-fi
+ADDITIONAL_BUILD_ARGS=""
 
 if [ -n "$GITHUB_TOKEN" ]; then
     # Use the github token to authenticate the git interaction (see this Stackoverflow answer: https://stackoverflow.com/a/57229018/5379273)
     git config --global url."https://api:$GITHUB_TOKEN@github.com/".insteadOf "https://github.com/"
     git config --global url."https://ssh:$GITHUB_TOKEN@github.com/".insteadOf "ssh://git@github.com/"
     git config --global url."https://git:$GITHUB_TOKEN@github.com/".insteadOf "git@github.com:"
+
+    ADDITIONAL_BUILD_ARGS="$ADDITIONAL_BUILD_ARGS --github-token=$GITHUB_TOKEN"
 fi
 
 if [ -n "$INPUT_CONTAINER_REGISTRY_USERNAME" ] && [ -n "$INPUT_CONTAINER_REGISTRY_PASSWORD" ]; then
     docker login $INPUT_CONTAINER_REGISTRY_URL -u "$INPUT_CONTAINER_REGISTRY_USERNAME" -p "$INPUT_CONTAINER_REGISTRY_PASSWORD"
+    ADDITIONAL_BUILD_ARGS="$ADDITIONAL_BUILD_ARGS --container-registry-url=$INPUT_CONTAINER_REGISTRY_URL"
+    ADDITIONAL_BUILD_ARGS="$ADDITIONAL_BUILD_ARGS --container-registry-username=$INPUT_CONTAINER_REGISTRY_USERNAME"
+    ADDITIONAL_BUILD_ARGS="$ADDITIONAL_BUILD_ARGS --container-registry-password=$INPUT_CONTAINER_REGISTRY_PASSWORD"
 fi
 
 # Navigate to working directory, if provided
@@ -31,6 +33,14 @@ if [ -z "$INPUT_BUILD_ARGS" ]; then
     INPUT_BUILD_ARGS="--make --test"
 fi
 
+if [ -n "$INPUT_PYPI_TOKEN" ]; then
+    ADDITIONAL_BUILD_ARGS="$ADDITIONAL_BUILD_ARGS --pypi-token=$INPUT_PYPI_TOKEN"
+fi
+
+if [ -n "$INPUT_PYPI_TEST_TOKEN" ]; then
+    ADDITIONAL_BUILD_ARGS="$ADDITIONAL_BUILD_ARGS --pypi-test-token=$INPUT_PYPI_TEST_TOKEN"
+fi
+
 printenv
 pwd
-python -u build.py $INPUT_BUILD_ARGS
+echo "python -u build.py $INPUT_BUILD_ARGS $ADDITIONAL_BUILD_ARGS"
