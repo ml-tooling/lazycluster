8a2722414a97e98fcfcccfc50084717974609e35
Jan Kalkan <jan.kalkan@mailbox.org>
2020-10-29 11:48:16 +0100

    Remove comment

--- src/lazycluster/__init__.py
+++ src/lazycluster/__init__.py
@@ -5,8 +5,6 @@ __version__ = about.__version__
 from lazycluster.exceptions import *
 from lazycluster.runtime_mgmt import RuntimeGroup, RuntimeManager
 from lazycluster.runtimes import Runtime, RuntimeTask
-
-# from .settings import BRANCH, GITHUB_URL, PIP_PROJECT_NAME
 from lazycluster.utils import Environment
 
 Environment.set_third_party_log_level(Environment.third_party_log_level)
