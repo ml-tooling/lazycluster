e2f09be877e662b87cfad7f739db912e0ef3379c
Jan Kalkan <jan.kalkan@mailbox.org>
2020-11-06 11:18:37 +0100

    Update build-env gh action

--- .github/actions/build-environment/entrypoint.sh
+++ .github/actions/build-environment/entrypoint.sh
@@ -3,7 +3,11 @@
 # Stops script execution if a command has an error
 set -e
 
-ADDITIONAL_BUILD_ARGS=""
+# set default build args if not provided
+BUILD_ARGS="$INPUT_BUILD_ARGS"
+if [ -z "$BUILD_ARGS" ]; then
+    BUILD_ARGS="--check --make --test"
+fi
 
 if [ -n "$GITHUB_TOKEN" ]; then
     # Use the github token to authenticate the git interaction (see this Stackoverflow answer: https://stackoverflow.com/a/57229018/5379273)
@@ -11,14 +15,14 @@ if [ -n "$GITHUB_TOKEN" ]; then
     git config --global url."https://ssh:$GITHUB_TOKEN@github.com/".insteadOf "ssh://git@github.com/"
     git config --global url."https://git:$GITHUB_TOKEN@github.com/".insteadOf "git@github.com:"
 
-    ADDITIONAL_BUILD_ARGS="$ADDITIONAL_BUILD_ARGS --github-token=$GITHUB_TOKEN"
+    BUILD_ARGS="$BUILD_ARGS --github-token=$GITHUB_TOKEN"
 fi
 
 if [ -n "$INPUT_CONTAINER_REGISTRY_USERNAME" ] && [ -n "$INPUT_CONTAINER_REGISTRY_PASSWORD" ]; then
     docker login $INPUT_CONTAINER_REGISTRY_URL -u "$INPUT_CONTAINER_REGISTRY_USERNAME" -p "$INPUT_CONTAINER_REGISTRY_PASSWORD"
-    ADDITIONAL_BUILD_ARGS="$ADDITIONAL_BUILD_ARGS --container-registry-url=$INPUT_CONTAINER_REGISTRY_URL"
-    ADDITIONAL_BUILD_ARGS="$ADDITIONAL_BUILD_ARGS --container-registry-username=$INPUT_CONTAINER_REGISTRY_USERNAME"
-    ADDITIONAL_BUILD_ARGS="$ADDITIONAL_BUILD_ARGS --container-registry-password=$INPUT_CONTAINER_REGISTRY_PASSWORD"
+    BUILD_ARGS="$BUILD_ARGS --container-registry-url=$INPUT_CONTAINER_REGISTRY_URL"
+    BUILD_ARGS="$BUILD_ARGS --container-registry-username=$INPUT_CONTAINER_REGISTRY_USERNAME"
+    BUILD_ARGS="$BUILD_ARGS --container-registry-password=$INPUT_CONTAINER_REGISTRY_PASSWORD"
 fi
 
 # Navigate to working directory, if provided
@@ -28,23 +32,29 @@ else
     cd $GITHUB_WORKSPACE
 fi
 
-# set default build args if not provided
-if [ -z "$INPUT_BUILD_ARGS" ]; then
-    INPUT_BUILD_ARGS="--make --test"
-fi
-
 if [ -n "$INPUT_PYPI_TOKEN" ]; then
-    ADDITIONAL_BUILD_ARGS="$ADDITIONAL_BUILD_ARGS --pypi-token=$INPUT_PYPI_TOKEN"
+    BUILD_ARGS="$BUILD_ARGS --pypi-token=$INPUT_PYPI_TOKEN"
 fi
 
-if [ -n "$INPUT_PYPI_TEST_TOKEN" ]; then
-    ADDITIONAL_BUILD_ARGS="$ADDITIONAL_BUILD_ARGS --pypi-test-token=$INPUT_PYPI_TEST_TOKEN"
+if [ -n "$INPUT_PYPI_REPOSITORY" ]; then
+    BUILD_ARGS="$BUILD_ARGS --pypi-repository=$INPUT_PYPI_REPOSITORY"
 fi
 
-if [ -n "$INPUT_BUILD_ARGS" ] && [[ $INPUT_BUILD_ARGS == *"--test"* ]]; then
-    pip install -e .
+# Get docker mount directory either via volume or bind mount
+INPUT_CONTAINER_MOUNT=$(echo $(docker inspect $(basename "$(cat /proc/1/cpuset)") | jq -r '.[0].HostConfig.Binds[]| select(. | contains("/github/workspace"))') | sed 's/:.*//')
+if [ -z "$INPUT_CONTAINER_MOUNT" ]; then
+    INPUT_CONTAINER_MOUNT=$(docker inspect $(basename "$(cat /proc/1/cpuset)") | jq -r '.[0].HostConfig.Mounts[]|select(.Target=="/github").Source')
+fi
+export INPUT_CONTAINER_MOUNT
+
+if [ -n "$INPUT_CONTAINER_MOUNT" ]; then
+    BUILD_ARGS="$BUILD_ARGS --container-mount=\"$INPUT_CONTAINER_MOUNT\""
 fi
 
-printenv
-pwd
-python -u build.py $INPUT_BUILD_ARGS $ADDITIONAL_BUILD_ARGS
+# Print command
+set -x
+
+pip install /github/workspace
+
+# Execute build script
+python -u build.py $BUILD_ARGS
