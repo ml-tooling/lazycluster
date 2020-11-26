cda498b30cadf7db1d0324eab2ce7bebaa2ce507
Jan Kalkan <jan.kalkan@mailbox.org>
2020-11-26 15:32:04 +0100

    Update cluster package init

--- src/lazycluster/cluster/__init__.py
+++ src/lazycluster/cluster/__init__.py
@@ -1,14 +1,10 @@
 from __future__ import absolute_import
 
-from lazycluster.cluster.runtime_cluster import (
-    MasterLauncher,
-    WorkerLauncher,
-    RuntimeCluster,
-    MasterWorkerCluster,
-)
-
 from lazycluster.cluster.dask_cluster import DaskCluster
 from lazycluster.cluster.hyperopt_cluster import HyperoptCluster
-
-# from lazycluster.cluster.ray_cluster import RayCluster
-
+from lazycluster.cluster.runtime_cluster import (
+    MasterLauncher,
+    MasterWorkerCluster,
+    RuntimeCluster,
+    WorkerLauncher,
+)
